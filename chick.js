39e18728998351c1a61d5e78397aaac74193d204
Dylan <Dylan.Griff11@gmail.com>
2019-08-28 10:47:30 -0500

    first commit

---
+++ chick.js
@@ -0,0 +1,14 @@
+
+/* chickenMonkey*/
+
+for (let i= 1; i <= 100; i++) {
+ if ( i % 5 === 0 && i % 7 === 0) {
+   console.log('ChickenMonkey');   
+} else if (i % 7 === 0) {
+   console.log('Monkey');
+} else if (i % 5  === 0) {
+   console.log('Chicken');
+} else {
+  console.log(i);
+} 
+}
